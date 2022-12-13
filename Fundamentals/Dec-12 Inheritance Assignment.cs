da7d6e153061ee38317d12aceaabd0c765178c25
gautamcha <gautmcha@gmail.com>
2022-12-12 17:56:18 -0700

    Inheritance Assignment

---
+++ Fundamentals/Dec-12 Inheritance Assignment.cs
@@ -0,0 +1,33 @@
+interface ILoanPayment
+{
+    public float NumberOfYears();
+    public float MonthlyInterest();
+    public float MonthlyPayment();   
+}
+
+class HomeLoan: ILoanPayment
+{
+    public HomeLoan(float i, float t, float p)
+    {
+        interest = i;
+        term = t;
+        principle = p;
+    }
+
+    static float interest;
+    static float term;
+    float principle;
+    float mi = interest/12;
+    float mt = term * 12;
+
+    public float NumberOfYears() => term * 12 * -1;
+    public float MonthlyInterest() => interest/12;
+    public float MonthlyPayment() => (mi * principle) / (1 - Math.Pow(1 + mi, mt * -1));
+}
+
+class CarLoan : HomeLoan
+{
+    public CarLoan(float i, float t, float p) : base(i, t, p)
+    {
+    }
+}
