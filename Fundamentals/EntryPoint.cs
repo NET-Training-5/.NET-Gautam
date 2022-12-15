d9656345d048a21af278ab2a5b35ebe290fa9b1b
gautamcha <gautmcha@gmail.com>
2022-12-15 15:46:22 -0700

    Learn About Selection Statements

--- Fundamentals/EntryPoint.cs
+++ Fundamentals/EntryPoint.cs
@@ -2,13 +2,6 @@ class EntryPoint
 {
     public static void Main()
     {
-        //FiFa worldcup2020 = new();
-        //worldcup2020.host = "";
-        //worldcup2020.year = new DateTime(2018, 2, 3);
-        //worldcup2020.winner = "France";
-        //worldcup2020.teams = new string[] {"France", "Germany", "Brazil"};
-
-        //FiFa worldcup2022 = new("Qatar");
         MakeDirectory df = new();
         df.CreateTenDirectories();
     }
