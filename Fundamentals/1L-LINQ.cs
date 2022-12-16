5e533fd4c67f355512a327757e1da36f9532e4de
gautamcha <gautmcha@gmail.com>
2022-12-15 17:58:36 -0700

    Learn About LINQ

--- Fundamentals/1L-LINQ.cs
+++ Fundamentals/1L-LINQ.cs
@@ -1,27 +1,54 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 class LINQ
 {
-    int[] numbers = {3, 5, 8, 4, 9, 7, 34};
+    int[] numbers = {3, 5, 99, 100, 36, 70, 65, 60, 110, 51, 35, 8, 4, 9, 7, 34};
     
-    void LearnToQueary()
+    public void LearnToQuery()
     {
         //List all even numbers from "numbers" array
 
         //This is Imperative way
-        List<int> evens = new List<int>();
-        foreach(var num in numbers)
-        {
-            if(num / 2 == 0)
-            {
-                evens.Add(num);
-            }    
-        }
+        //List<int> evens = new List<int>();
+        //foreach(var num in numbers)
+        //{
+        //    if(num / 2 == 0)
+        //   {
+        //       evens.Add(num);
+        //    }    
+        //}
         // Declarative way of doing things.
-        var evenNumbers = numbers.Where(num => num / 2 == 0);
+        var evenNumbers = numbers.Where(x => x / 2 == 0);
+
+        foreach (int x in evenNumbers)
+        {
+            Console.WriteLine(x);
+        } 
 
         // List all odd numbers which are divisible by 3
         var oddNumbers = numbers.Where(x => x % 2 != 0 && x % 3 == 0);
+
+        foreach (int x in oddNumbers)
+        {
+            Console.WriteLine(x);
+        } 
+
+        // List multiples of 5 and 7 from "numbers"
+        var multipleOf5N7 = numbers.Where(x => x / 5 == 0 && x / 7 == 0);
+
+        foreach (int x in multipleOf5N7)
+        {
+            Console.WriteLine(x);
+        } 
+
+        // List all items less than 100 and ends with 0, from "numbers"
+        var lessThanHunNEndWithZero = numbers.Where(x => x < 100 && x / 10 == 0);
+
+        foreach (int x in lessThanHunNEndWithZero)
+        {
+            Console.WriteLine(x);
+        } 
     }
 }
