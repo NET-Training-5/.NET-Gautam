63aa6cbdaa848a01b3e6138ae30ad07c9e25cd9e
gautamcha <gautmcha@gmail.com>
2022-12-27 17:02:41 -0700

    Updated Employee Table

--- Web Development/HelloWeb/Controllers/EmployeeController.cs
+++ Web Development/HelloWeb/Controllers/EmployeeController.cs
@@ -4,6 +4,34 @@ public class EmployeeController: Controller
 {
     public IActionResult EmployeeDetails()
     {
-        return View();
+        Employee e1 = new Employee()
+        {
+        Fullname = "Biswas Shrestha",
+        Sex = Gender.Male,
+        JoinDate = new DateTime(2010, 7, 12),
+        Designation = "Software Engineer",
+        Department= "IT"
+        };
+
+        Employee e2 = new Employee()
+        {
+        Fullname = "Sarita Shrestha",
+        Sex = Gender.Female,
+        JoinDate = new DateTime(2009, 7, 12),
+        Designation = "Sr. Software Engineer",
+        Department= "IT",
+        };
+
+        Employee e3 = new Employee()
+        {
+        Fullname = "Ram Karki",
+        Sex = Gender.Male,
+        JoinDate = new DateTime(2007, 7, 12),
+        Designation = "Business Analyst",
+        Department= "Business/Technical",
+        };
+
+        List<Employee> employees = new List<Employee>() { e1, e2, e3};
+        return View(employees);
     }
 }    
---
+++ Web Development/HelloWeb/Models/Employee.cs
@@ -0,0 +1,8 @@
+public class Employee
+{
+    public string Fullname {get; set; }
+    public string Designation {get; set; }
+    public Gender Sex {get; set; }
+    public DateTime JoinDate {get; set; }
+    public string Department {get; set; }
+}
